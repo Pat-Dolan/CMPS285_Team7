0bb496e3f841733ef79ae982c1be0dcee019bc84
Jonathan Torregano <jonathan.torregano@selu.edu>
2015-10-10 21:36:44 -0500

    Ok so here are the Files I modified for our Facebook Login. Anything I didn't type/need I commented out. Other Commented Out code is for the pulling of information from facebook, which  I wasn't able finish before the freeze.
    -Users are able to log in successfully and generate a user token, seeable in our console window (user unique so no sharing)
    -Agreeable Permission window pops up saying we will need access to friends list.
    -If User cancels any, they're pushed back to login screen and are forced to re sign in.
    -Solo Login Button

--- AutomaTech/AutomaTech/Activities/LoginActivity.cs
+++ AutomaTech/AutomaTech/Activities/LoginActivity.cs
@@ -8,39 +8,114 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-//using Xamarin.Facebook;
+using Xamarin.Facebook;
+using Xamarin.Facebook.Login;
+using Xamarin.Facebook.Login.Widget;
 
 namespace AutomaTech
 {
 	
 	[Activity (Label = "TourPlus+", MainLauncher = true, Icon = "@drawable/Icon")]			
-	public class LoginActivity : Activity
+	public class LoginActivity : Activity, IFacebookCallback
 	{
 		//Declaring variables that are accessed in functions
 		GlobalVars g = GlobalVars.getInstance();
+
+		private ICallbackManager mCallBackManager;
+		public myProfileTracker mProfileTracker;
+
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
-			//This Function will Allow users to Register Via FaceBook
-			//FacebookSdk.SdkInitialize(this.ApplicationContext); 
+			FacebookSdk.SdkInitialize (this.ApplicationContext); //This Function will Allow users to Register Via FaceBook
 
 			//Setting layout
-			SetContentView(Resource.Layout.LoginLayout);
+			SetContentView (Resource.Layout.LoginLayout);
 
-			//Opening database connection for testing login info
-			DBRepository dbr = new DBRepository ();
-			var result = dbr.CreateDB ();						//returns testing string
-			//Toast.MakeText (this, result.ToString(), ToastLength.Short).Show ();		//displaying testing value
+			mProfileTracker = new myProfileTracker ();
+			mProfileTracker.StartTracking ();
 
-			Button tempLogin = FindViewById<Button> (Resource.Id.tempLogin);
-			tempLogin.Click += TempLogin_Click;
+			LoginButton button = FindViewById<LoginButton> (Resource.Id.login_button);
+
+			button.SetReadPermissions ("user_friends");
+			mCallBackManager = CallbackManagerFactory.Create ();
+			button.RegisterCallback (mCallBackManager, this);
 		}
-		void TempLogin_Click (object sender, EventArgs e)
+
+		public void OnCancel ()
 		{
+			//throw new NotImplementedException ();
+		}
+
+		public void OnError (FacebookException error)
+		{
+			//throw new NotImplementedException ();
+		}
+
+		public void OnSuccess (Java.Lang.Object result)
+		{
+			LoginResult loginResult = result as LoginResult; 
+			Console.WriteLine(loginResult.AccessToken.UserId); //Token is the Unique Key for UserFacebooks
 			StartActivity (typeof(MainActivity));
 		}
+
+		protected override void OnActivityResult (int requestCode, Result resultCode, Intent data)
+		{
+			base.OnActivityResult (requestCode, resultCode, data);
+			mCallBackManager.OnActivityResult (requestCode, (int)resultCode, data);
+		}
+
+		protected override void OnDestroy ()
+		{
+			mProfileTracker.StopTracking();
+			base.OnDestroy();
+		}
+	}
+
+	public class myProfileTracker : ProfileTracker
+	{
+		public event EventHandler<OnProfileChangedEventArgs> mOnProfileChanged;
+		protected override void OnCurrentProfileChanged(Profile oldProfile, Profile newProfile)
+		{
+			if (mOnProfileChanged != null) 
+			{
+				mOnProfileChanged.Invoke (this, new OnProfileChangedEventArgs (newProfile));
+			}
+		}
+	}
+
+	public class OnProfileChangedEventArgs : EventArgs
+	{
+		public Profile mProfile;
+		public OnProfileChangedEventArgs (Profile profile) {mProfile = profile;}
+	}
+
+	public static class GlobalVar
+	{
+		public const int userProfile = AccessToken.CurrentAccessToken;
 	}
 }
+//
+//		protected override void OnDestroy()
+//		{
+//			mProfileTracker.StopTracking ();
+//			base.OnDestroy ();
+//		}
+//
+//			//Opening database connection for testing login info
+//			DBRepository dbr = new DBRepository ();
+//			var result = dbr.CreateDB ();						//returns testing string
+//			//Toast.MakeText (this, result.ToString(), ToastLength.Short).Show ();		//displaying testing value
+//
+//			Button tempLogin = FindViewById<Button> (Resource.Id.tempLogin);
+//			tempLogin.Click += TempLogin_Click;
+//		}
+//		void TempLogin_Click (object sender, EventArgs e)
+//		{
+//			StartActivity (typeof(MainActivity));
+//		}
+//	}
+//}
 
